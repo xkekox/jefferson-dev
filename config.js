8cd404203120ce96be4a5e9d41bcedbb886951cc
xkekox <jefersonkeko15@gmail.com>
2026-03-21 21:07:18 -0300

    chore: publica config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__SUPABASE_CONFIG__ = window.__SUPABASE_CONFIG__ || {
-  url: "",
-  anonKey: "",
+  url: "https://ixnlonpywqxkfbqolilt.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4bmxvbnB5d3F4a2ZicW9saWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NTQ4MzUsImV4cCI6MjA4OTQzMDgzNX0.4aanRI5EEavExlN7hSlkJ8COlROt6INWLKhSfhJSYCs",
   projectScope: "zain-pichau-console"
 };
